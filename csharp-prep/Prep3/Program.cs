4410665d91c84f00b8442edb01b6d7c1332a2c5c
Anton Sanakoev <totum42@gmail.com>
2024-06-19 17:03:47 +0400

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("My number between 1 and 101.");
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
