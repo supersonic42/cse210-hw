6c3b9cf3c2d2ea8525841c4064515fca8c70ae82
Anton Sanakoev <totum42@gmail.com>
2024-06-26 17:35:10 +0400

    Fraction class added

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+        Console.WriteLine($"top: {f4.GetTop()} == 1; bottom: {f4.GetBottom()} == 3");
+
+        f4.SetTop(10);
+        f4.SetBottom(100);
+        Console.WriteLine($"top: {f4.GetTop()} == 10; bottom: {f4.GetBottom()} == 100");
     }
 }
