3a3c7704db1dc41d9eb6b2c6e3b72e32da9b4b3f
Anton Sanakoev <totum42@gmail.com>
2024-07-17 17:56:07 +0400

    Comment about creativity

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -3,7 +3,9 @@ Project: Eternal Quest
 Author: Anton Sanakoev
 
 Exceeding requirements:
-1. 
+1. Instead of keeping total score of the Goal Manager in the file, i decided to count it dynamically:
+Each time file is loaded, program runs throughs all goals and calculates total score by calling CalculatePoints() 
+function for each goal.
 */
 
 using System;
